d4516f3d5f2dc4ac63660efbdd431ef05a986c36
BrianLohWJ <75010149+BrianLohWJ@users.noreply.github.com>
2025-09-17 00:05:06 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,125 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-reception-area",
+      "name": "Reception Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3145705460316273
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4356482526976073,
+          "pitch": 1.3783228322065213,
+          "rotation": 0,
+          "target": "0-reception-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-walkway",
+      "name": "Walkway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.004610430765740858,
+        "pitch": -0.03851594449171891,
+        "fov": 1.3145705460316273
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.32624753661523087,
+          "pitch": 1.3015513662601847,
+          "rotation": 6.283185307179586,
+          "target": "1-walkway"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-parking-lot",
+      "name": "Parking Lot",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.007433916291589071,
+        "pitch": -0.01572054938267442,
+        "fov": 1.3145705460316273
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.666524517123147,
+          "pitch": 1.4379463409635829,
+          "rotation": 0,
+          "target": "2-parking-lot"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
